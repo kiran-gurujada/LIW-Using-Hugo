ecef8703da122db5bc96bb3c276094f8b9095034
Kiran Chirravuri <kiran@gurujada.org>
2016-10-29 11:45:04 +0530

    first commit

---
+++ themes/liw/static/js/contactform.js
@@ -0,0 +1,65 @@
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#cont").submit(function(event){
+swal({   title: "Success",   text: "Thank you for getting in touch. We try to respond as soon as possible.",   imageUrl: "images/contact.png" });
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbyFoIW5SfK8Y6TNAAke24avumf3D1gkp5rb7WSSRK1FpTnbC2yt/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        ClearForm();
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+    function ClearForm() {
+         document.getElementById("name").value = "";
+         document.getElementById("Email").value = "";
+         document.getElementById("subject").value = "";
+         document.getElementById("message").value = "";
+    }
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
